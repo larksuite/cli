3b4d092c0b88581995194e25dc5d126aaa654f4c
calendar-assistant <tangfengyuan@bytedance.com>
2026-04-07 21:35:42 +0800

    docs: fix root calendar example
    Change-Id: I25e58345019046bf961636b10e8baad522019279

--- cmd/root.go
+++ cmd/root.go
@@ -45,7 +45,7 @@ EXAMPLES:
     lark-cli calendar +agenda
 
     # List calendar events
-    lark-cli calendar events list --params '{"calendar_id":"primary"}'
+    lark-cli calendar events instance_view --params '{"calendar_id":"primary","start_time":"1700000000","end_time":"1700086400"}'
 
     # Search users
     lark-cli contact +search-user --query "John"
