c7aec81a0fb26ea68f7fc0f9c98b0e5b222b66e4
shanglei <shanglei.ops@bytedance.com>
2026-04-18 13:56:43 +0800

    feat(sidecar): fix CI

--- main_authsidecar.go
+++ main_authsidecar.go
@@ -6,6 +6,6 @@
 package main
 
 import (
-	_ "github.com/larksuite/cli/extension/credential/sidecar"  // activate sidecar credential provider
-	_ "github.com/larksuite/cli/extension/transport/sidecar"    // activate sidecar transport interceptor
+	_ "github.com/larksuite/cli/extension/credential/sidecar" // activate sidecar credential provider
+	_ "github.com/larksuite/cli/extension/transport/sidecar"  // activate sidecar transport interceptor
 )
