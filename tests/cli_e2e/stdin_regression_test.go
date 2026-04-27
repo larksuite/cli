6e9eddcc0e8d2fcf1f1e898506b52cc242e1ad20
zgz2048 <zhonggangzhi.tim@bytedance.com>
2026-04-27 16:39:38 +0800

    test: isolate dry-run e2e config state

--- tests/cli_e2e/stdin_regression_test.go
+++ tests/cli_e2e/stdin_regression_test.go
@@ -197,6 +197,7 @@ func TestCLIStdinRegression_ErrorCases(t *testing.T) {
 
 func setDryRunConfigEnv(t *testing.T) {
 	t.Helper()
+	t.Setenv("LARKSUITE_CLI_CONFIG_DIR", t.TempDir())
 	t.Setenv("LARKSUITE_CLI_APP_ID", "app")
 	t.Setenv("LARKSUITE_CLI_APP_SECRET", "secret")
 	t.Setenv("LARKSUITE_CLI_BRAND", "feishu")
