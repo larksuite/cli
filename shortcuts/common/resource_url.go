CloudEngineHub/lark-cli

feat(common): backfill resource URL when create APIs omit it
Add BuildResourceURL helper and wire it into doc/sheets/drive/base/wiki
create paths so callers always receive a clickable link, even when the
backend response (MCP degraded path or upstream OpenAPI) returns an
empty URL field. The fallback uses the brand-standard host
(www.feishu.cn / www.larksuite.com), which redirects to the tenant
domain.

Affected entries:
- docs +create v1 / v2
- sheets +create
- drive +create-folder / +import / +upload (newly exposes url)
- base +base-create / +base-copy
- wiki +node-create (newly exposes url)

drive +create-shortcut is intentionally skipped because the URL form
depends on the underlying file kind, which the shortcut payload does
not carry.

shortcuts/common/resource_url.go
@@ -1,0 +1,53 @@
+// Copyright (c) 2026 Lark Technologies Pte. Ltd.
+// SPDX-License-Identifier: MIT
+
+package common
+
+import (
+	"strings"
+
+	"github.com/larksuite/cli/internal/core"
+)
+
+// BuildResourceURL returns a brand-standard, user-facing URL for a freshly
+// created Lark resource. It is intended as a fallback when the create API does
+// not return a URL field (e.g. drive +upload, wiki +node-create) or when the
+// returned URL is empty (e.g. degraded MCP responses for docs +create v1).
+//
+// The returned URL points at the brand's standard host (www.feishu.cn /
+// www.larksuite.com), which transparently redirects to the tenant-specific
+// domain. It is NOT a guess at the tenant's vanity domain.
+//
+// Returns "" when token is empty or kind is unrecognized — callers should
+// only set the field when the result is non-empty so that "" never overrides
+// a real URL the backend already returned.
+func BuildResourceURL(brand core.LarkBrand, kind, token string) string {
+	token = strings.TrimSpace(token)
+	if token == "" {
+		return ""
+	}
+
+	host := "https://www.feishu.cn"
+	if brand == core.BrandLark {
+		host = "https://www.larksuite.com"
+	}
+
+	switch strings.ToLower(strings.TrimSpace(kind)) {
+	case "docx":
+		return host + "/docx/" + token
+	case "doc":
+		return host + "/docs/" + token
+	case "sheet":
+		return host + "/sheets/" + token
+	case "bitable":
+		return host + "/base/" + token
+	case "wiki":
+		return host + "/wiki/" + token
+	case "file":
+		return host + "/file/" + token
+	case "folder":
+		return host + "/drive/folder/" + token
+	default:
+		return ""
+	}
+}

shortcuts/common/resource_url_test.go
@@ -1,0 +1,48 @@
+// Copyright (c) 2026 Lark Technologies Pte. Ltd.
+// SPDX-License-Identifier: MIT
+
+package common
+
+import (
+	"testing"
+
+	"github.com/larksuite/cli/internal/core"
+)
+
+func TestBuildResourceURL(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name  string
+		brand core.LarkBrand
+		kind  string
+		token string
+		want  string
+	}{
+		{"feishu docx", core.BrandFeishu, "docx", "doxcnABC", "https://www.feishu.cn/docx/doxcnABC"},
+		{"feishu doc legacy", core.BrandFeishu, "doc", "doccnABC", "https://www.feishu.cn/docs/doccnABC"},
+		{"feishu sheet", core.BrandFeishu, "sheet", "shtcnABC", "https://www.feishu.cn/sheets/shtcnABC"},
+		{"feishu bitable", core.BrandFeishu, "bitable", "bascnABC", "https://www.feishu.cn/base/bascnABC"},
+		{"feishu wiki", core.BrandFeishu, "wiki", "wikcnABC", "https://www.feishu.cn/wiki/wikcnABC"},
+		{"feishu file", core.BrandFeishu, "file", "boxcnABC", "https://www.feishu.cn/file/boxcnABC"},
+		{"feishu folder", core.BrandFeishu, "folder", "fldcnABC", "https://www.feishu.cn/drive/folder/fldcnABC"},
+		{"lark docx", core.BrandLark, "docx", "doxcnABC", "https://www.larksuite.com/docx/doxcnABC"},
+		{"lark wiki", core.BrandLark, "wiki", "wikcnABC", "https://www.larksuite.com/wiki/wikcnABC"},
+		{"empty brand defaults to feishu", core.LarkBrand(""), "docx", "doxcnABC", "https://www.feishu.cn/docx/doxcnABC"},
+		{"kind case-insensitive", core.BrandFeishu, "DOCX", "doxcnABC", "https://www.feishu.cn/docx/doxcnABC"},
+		{"token whitespace trimmed", core.BrandFeishu, "docx", "  doxcnABC  ", "https://www.feishu.cn/docx/doxcnABC"},
+		{"empty token", core.BrandFeishu, "docx", "", ""},
+		{"whitespace-only token", core.BrandFeishu, "docx", "   ", ""},
+		{"unknown kind", core.BrandFeishu, "mindnote", "mncnABC", ""},
+		{"empty kind", core.BrandFeishu, "", "doxcnABC", ""},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := BuildResourceURL(tt.brand, tt.kind, tt.token)
+			if got != tt.want {
+				t.Errorf("BuildResourceURL(%q, %q, %q) = %q, want %q", tt.brand, tt.kind, tt.token, got, tt.want)
+			}
+		})
+	}
+}

larksuite/cli

shortcuts/common/resource_url.go
@@ -1,0 +1,57 @@
+// Copyright (c) 2026 Lark Technologies Pte. Ltd.
+// SPDX-License-Identifier: MIT
+
+package common
+
+import (
+	"strings"
+
+	"github.com/larksuite/cli/internal/core"
+)
+
+// BuildResourceURL returns a brand-standard, user-facing URL for a freshly
+// created Lark resource. It is intended as a fallback when the create API does
+// not return a URL field (e.g. drive +upload, wiki +node-create) or when the
+// returned URL is empty (e.g. degraded MCP responses for docs +create v1).
+//
+// The returned URL points at the brand's standard host (www.feishu.cn /
+// www.larksuite.com), which transparently redirects to the tenant-specific
+// domain. It is NOT a guess at the tenant's vanity domain.
+//
+// Returns "" when token is empty or kind is unrecognized — callers should
+// only set the field when the result is non-empty so that "" never overrides
+// a real URL the backend already returned.
+func BuildResourceURL(brand core.LarkBrand, kind, token string) string {
+	token = strings.TrimSpace(token)
+	if token == "" {
+		return ""
+	}
+
+	host := "https://www.feishu.cn"
+	if brand == core.BrandLark {
+		host = "https://www.larksuite.com"
+	}
+
+	switch strings.ToLower(strings.TrimSpace(kind)) {
+	case "docx":
+		return host + "/docx/" + token
+	case "doc":
+		return host + "/doc/" + token
+	case "sheet":
+		return host + "/sheets/" + token
+	case "bitable":
+		return host + "/base/" + token
+	case "wiki":
+		return host + "/wiki/" + token
+	case "file":
+		return host + "/file/" + token
+	case "folder":
+		return host + "/drive/folder/" + token
+	case "mindnote":
+		return host + "/mindnote/" + token
+	case "slides":
+		return host + "/slides/" + token
+	default:
+		return ""
+	}
+}

shortcuts/common/resource_url_test.go
@@ -1,0 +1,50 @@
+// Copyright (c) 2026 Lark Technologies Pte. Ltd.
+// SPDX-License-Identifier: MIT
+
+package common
+
+import (
+	"testing"
+
+	"github.com/larksuite/cli/internal/core"
+)
+
+func TestBuildResourceURL(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name  string
+		brand core.LarkBrand
+		kind  string
+		token string
+		want  string
+	}{
+		{"feishu docx", core.BrandFeishu, "docx", "doxcnABC", "https://www.feishu.cn/docx/doxcnABC"},
+		{"feishu doc legacy", core.BrandFeishu, "doc", "doccnABC", "https://www.feishu.cn/doc/doccnABC"},
+		{"feishu sheet", core.BrandFeishu, "sheet", "shtcnABC", "https://www.feishu.cn/sheets/shtcnABC"},
+		{"feishu bitable", core.BrandFeishu, "bitable", "bascnABC", "https://www.feishu.cn/base/bascnABC"},
+		{"feishu wiki", core.BrandFeishu, "wiki", "wikcnABC", "https://www.feishu.cn/wiki/wikcnABC"},
+		{"feishu file", core.BrandFeishu, "file", "boxcnABC", "https://www.feishu.cn/file/boxcnABC"},
+		{"feishu folder", core.BrandFeishu, "folder", "fldcnABC", "https://www.feishu.cn/drive/folder/fldcnABC"},
+		{"feishu mindnote", core.BrandFeishu, "mindnote", "mncnABC", "https://www.feishu.cn/mindnote/mncnABC"},
+		{"feishu slides", core.BrandFeishu, "slides", "slkcnABC", "https://www.feishu.cn/slides/slkcnABC"},
+		{"lark docx", core.BrandLark, "docx", "doxcnABC", "https://www.larksuite.com/docx/doxcnABC"},
+		{"lark wiki", core.BrandLark, "wiki", "wikcnABC", "https://www.larksuite.com/wiki/wikcnABC"},
+		{"empty brand defaults to feishu", core.LarkBrand(""), "docx", "doxcnABC", "https://www.feishu.cn/docx/doxcnABC"},
+		{"kind case-insensitive", core.BrandFeishu, "DOCX", "doxcnABC", "https://www.feishu.cn/docx/doxcnABC"},
+		{"token whitespace trimmed", core.BrandFeishu, "docx", "  doxcnABC  ", "https://www.feishu.cn/docx/doxcnABC"},
+		{"empty token", core.BrandFeishu, "docx", "", ""},
+		{"whitespace-only token", core.BrandFeishu, "docx", "   ", ""},
+		{"unknown kind", core.BrandFeishu, "calendar", "calABC", ""},
+		{"empty kind", core.BrandFeishu, "", "doxcnABC", ""},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := BuildResourceURL(tt.brand, tt.kind, tt.token)
+			if got != tt.want {
+				t.Errorf("BuildResourceURL(%q, %q, %q) = %q, want %q", tt.brand, tt.kind, tt.token, got, tt.want)
+			}
+		})
+	}
+}