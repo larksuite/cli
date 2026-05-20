CloudEngineHub/lark-cli

feat: support wiki node target in markdown +create
Change-Id: Idb89464344599571cda3d27d136727553dcf0e7e

tests/cli_e2e/markdown/markdown_workflow_test.go
@@ -6,6 +6,7 @@ package markdown
 import (
 	"context"
 	"os"
+	"strings"
 	"testing"
 	"time"
 
@@ -97,4 +98,85 @@ func TestMarkdownLifecycleWorkflow(t *testing.T) {
 	fetchUpdatedResult.AssertExitCode(t, 0)
 	fetchUpdatedResult.AssertStdoutStatus(t, true)
 	require.Equal(t, updatedContent, gjson.Get(fetchUpdatedResult.Stdout, "data.content").String(), "stdout:\n%s", fetchUpdatedResult.Stdout)
+}
+
+func TestMarkdownCreateWorkflow_WikiParent(t *testing.T) {
+	if os.Getenv("LARK_MARKDOWN_E2E") == "" {
+		t.Skip("set LARK_MARKDOWN_E2E=1 to run markdown live workflow after backend version support is deployed")
+	}
+
+	wikiToken := strings.TrimSpace(os.Getenv("LARK_MARKDOWN_E2E_WIKI_TOKEN"))
+	if wikiToken == "" {
+		t.Skip("set LARK_MARKDOWN_E2E_WIKI_TOKEN to run markdown live workflow against a wiki parent node")
+	}
+
+	t.Setenv("LARKSUITE_CLI_CONFIG_DIR", t.TempDir())
+
+	parentT := t
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Minute)
+	t.Cleanup(cancel)
+
+	suffix := clie2e.GenerateSuffix()
+	fileName := "lark-cli-e2e-markdown-wiki-" + suffix + ".md"
+	initialContent := "# Wiki Parent\n\nhello wiki markdown workflow\n"
+
+	createResult, err := clie2e.RunCmd(ctx, clie2e.Request{
+		Args: []string{
+			"markdown", "+create",
+			"--wiki-token", wikiToken,
+			"--name", fileName,
+			"--content", initialContent,
+		},
+		DefaultAs: "bot",
+	})
+	require.NoError(t, err)
+	createResult.AssertExitCode(t, 0)
+	createResult.AssertStdoutStatus(t, true)
+
+	fileToken := gjson.Get(createResult.Stdout, "data.file_token").String()
+	require.NotEmpty(t, fileToken, "stdout:\n%s", createResult.Stdout)
+	require.False(t, gjson.Get(createResult.Stdout, "data.url").Exists(), "stdout:\n%s", createResult.Stdout)
+
+	parentT.Cleanup(func() {
+		bestEffortDeleteWikiHostedMarkdownFile(parentT, fileToken)
+	})
+
+	fetchResult, err := clie2e.RunCmd(ctx, clie2e.Request{
+		Args: []string{
+			"markdown", "+fetch",
+			"--file-token", fileToken,
+		},
+		DefaultAs: "bot",
+	})
+	require.NoError(t, err)
+	fetchResult.AssertExitCode(t, 0)
+	fetchResult.AssertStdoutStatus(t, true)
+	require.Equal(t, initialContent, gjson.Get(fetchResult.Stdout, "data.content").String(), "stdout:\n%s", fetchResult.Stdout)
+}
+
+func bestEffortDeleteWikiHostedMarkdownFile(parentT *testing.T, fileToken string) {
+	parentT.Helper()
+
+	request := clie2e.Request{
+		Args: []string{
+			"drive", "+delete",
+			"--file-token", fileToken,
+			"--type", "file",
+			"--yes",
+		},
+	}
+
+	for _, identity := range []string{"bot", "user"} {
+		cleanupCtx, cleanupCancel := clie2e.CleanupContext()
+		result, err := clie2e.RunCmd(cleanupCtx, clie2e.Request{
+			Args:      request.Args,
+			DefaultAs: identity,
+		})
+		cleanupCancel()
+		if err == nil && result != nil && result.ExitCode == 0 {
+			return
+		}
+	}
+
+	parentT.Logf("cleanup skipped: could not delete wiki-hosted markdown file %s with either bot or user identity", fileToken)
 }

larksuite/cli

tests/cli_e2e/markdown/markdown_workflow_test.go
@@ -181,3 +181,84 @@ func TestMarkdownLifecycleWorkflow(t *testing.T) {
 	assert.True(t, strings.Contains(diffText, "-hello markdown workflow") || strings.Contains(diffText, "-# Initial"), "stdout:\n%s", diffResult.Stdout)
 	assert.True(t, strings.Contains(diffText, "+new body") || strings.Contains(diffText, "+# Updated"), "stdout:\n%s", diffResult.Stdout)
 }
+
+func TestMarkdownCreateWorkflow_WikiParent(t *testing.T) {
+	if os.Getenv("LARK_MARKDOWN_E2E") == "" {
+		t.Skip("set LARK_MARKDOWN_E2E=1 to run markdown live workflow after backend version support is deployed")
+	}
+
+	wikiToken := strings.TrimSpace(os.Getenv("LARK_MARKDOWN_E2E_WIKI_TOKEN"))
+	if wikiToken == "" {
+		t.Skip("set LARK_MARKDOWN_E2E_WIKI_TOKEN to run markdown live workflow against a wiki parent node")
+	}
+
+	t.Setenv("LARKSUITE_CLI_CONFIG_DIR", t.TempDir())
+
+	parentT := t
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Minute)
+	t.Cleanup(cancel)
+
+	suffix := clie2e.GenerateSuffix()
+	fileName := "lark-cli-e2e-markdown-wiki-" + suffix + ".md"
+	initialContent := "# Wiki Parent\n\nhello wiki markdown workflow\n"
+
+	createResult, err := clie2e.RunCmd(ctx, clie2e.Request{
+		Args: []string{
+			"markdown", "+create",
+			"--wiki-token", wikiToken,
+			"--name", fileName,
+			"--content", initialContent,
+		},
+		DefaultAs: "bot",
+	})
+	require.NoError(t, err)
+	createResult.AssertExitCode(t, 0)
+	createResult.AssertStdoutStatus(t, true)
+
+	fileToken := gjson.Get(createResult.Stdout, "data.file_token").String()
+	require.NotEmpty(t, fileToken, "stdout:\n%s", createResult.Stdout)
+	require.False(t, gjson.Get(createResult.Stdout, "data.url").Exists(), "stdout:\n%s", createResult.Stdout)
+
+	parentT.Cleanup(func() {
+		requireDeleteWikiHostedMarkdownFile(parentT, fileToken)
+	})
+
+	fetchResult, err := clie2e.RunCmd(ctx, clie2e.Request{
+		Args: []string{
+			"markdown", "+fetch",
+			"--file-token", fileToken,
+		},
+		DefaultAs: "bot",
+	})
+	require.NoError(t, err)
+	fetchResult.AssertExitCode(t, 0)
+	fetchResult.AssertStdoutStatus(t, true)
+	require.Equal(t, initialContent, gjson.Get(fetchResult.Stdout, "data.content").String(), "stdout:\n%s", fetchResult.Stdout)
+}
+
+func requireDeleteWikiHostedMarkdownFile(parentT *testing.T, fileToken string) {
+	parentT.Helper()
+
+	request := clie2e.Request{
+		Args: []string{
+			"drive", "+delete",
+			"--file-token", fileToken,
+			"--type", "file",
+			"--yes",
+		},
+	}
+
+	for _, identity := range []string{"bot", "user"} {
+		cleanupCtx, cleanupCancel := clie2e.CleanupContext()
+		result, err := clie2e.RunCmd(cleanupCtx, clie2e.Request{
+			Args:      request.Args,
+			DefaultAs: identity,
+		})
+		cleanupCancel()
+		if err == nil && result != nil && result.ExitCode == 0 {
+			return
+		}
+	}
+
+	parentT.Fatalf("cleanup failed: could not delete wiki-hosted markdown file %s with either bot or user identity", fileToken)
+}